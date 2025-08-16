8b2a06d1038afab0ce59f8e9a3efe543e0a85f80
KacperMalachowski <kacpermaachowski@gmail.com>
2025-08-16 22:09:23 +0200

    Add http server to main.go

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	srv := &http.Server {
+		Handler: r,
+		Addr:    ":8080",
+
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Println("Server started on :8080")
+	log.Fatal(srv.ListenAndServe())
 }
