9d08b0d7880654d7735631714ddcf3006e297892
KacperMalachowski <kacpermaachowski@gmail.com>
2025-08-17 19:03:37 +0200

    Working server

--- cmd/root.go
+++ cmd/root.go
@@ -2,10 +2,12 @@ package cmd
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 
 	"github.com/KacperMalachowski/todo-api/internal/db"
+	"github.com/KacperMalachowski/todo-api/internal/todos"
 	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
 )
@@ -48,6 +50,12 @@ func (s *server) AllTasks(w http.ResponseWriter, r *http.Request) {
 	}
 
 	resp, err := json.Marshal(tasks)
+	if err != nil {
+		s.logger.Printf("Error marshalling tasks", "error", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	_, err = w.Write(resp)
@@ -58,6 +66,84 @@ func (s *server) AllTasks(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (s *server) AddTask(w http.ResponseWriter, r *http.Request) {
+	var task todos.Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		s.logger.Printf("Error decoding task: %v", err)
+		http.Error(w, "Invalid task data", http.StatusBadRequest)
+		return
+	}
+
+	if err := s.database.Add(&task); err != nil {
+		s.logger.Printf("Error adding task: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (s *server) GetTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	task, err := s.database.Get(id)
+	if err != nil {
+		s.logger.Printf("Error getting task: %v", err)
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	resp, err := json.Marshal(task)
+	if err != nil {
+		s.logger.Printf("Error marshalling task: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = w.Write(resp)
+	if err != nil {
+		s.logger.Printf("Error writing response: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *server) UpdateTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var task todos.Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		s.logger.Printf("Error decoding task: %v", err)
+		http.Error(w, "Invalid task data", http.StatusBadRequest)
+		return
+	}
+
+	if err := s.database.Update(id, &task); err != nil {
+		if errors.Is(err, db.ErrNotFound) {
+			s.logger.Printf("Task not found: %v", err)
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+
+		s.logger.Printf("Error updating task: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (s *server) DeleteTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	s.database.Delete(id)
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func run(cmd *cobra.Command, args []string) error {
 	r := mux.NewRouter()
 	database := db.NewInMemory()
@@ -65,6 +151,10 @@ func run(cmd *cobra.Command, args []string) error {
 	server := NewServer(logger, database)
 
 	r.HandleFunc("/tasks", server.AllTasks).Methods("GET")
+	r.HandleFunc("/tasks", server.AddTask).Methods("POST")
+	r.HandleFunc("/tasks/{id}", server.GetTask).Methods("GET")
+	r.HandleFunc("/tasks/{id}", server.UpdateTask).Methods("PUT")
+	r.HandleFunc("/tasks/{id}", server.DeleteTask).Methods("DELETE")
 
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/KacperMalachowski/todo-api/internal/db"
@@ -13,6 +17,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+var logger = log.New(io.Discard, "", 0)
 
 // Test listing all tasks
 func TestListAllTasks(t *testing.T) {
@@ -32,7 +38,7 @@ func TestListAllTasks(t *testing.T) {
 
 	rr := httptest.NewRecorder()
 	router := mux.NewRouter()
-	router.HandleFunc("/tasks", NewServer(nil, database).AllTasks).Methods("GET")
+	router.HandleFunc("/tasks", NewServer(logger, database).AllTasks).Methods("GET")
 
 	router.ServeHTTP(rr, req)
 
@@ -46,5 +52,149 @@ func TestListAllTasks(t *testing.T) {
 
 	expectedTasks := []todos.Task{*task1, *task2}
 	assert.ElementsMatch(t, expectedTasks, tasks)
+}
+
+func TestListAllTasksEmpty(t *testing.T) {
+	database := db.NewInMemory()
+	require.NotNil(t, database)
+
+	req, err := http.NewRequest("GET", "/tasks", nil)
+	require.NoError(t, err)
+
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc("/tasks", NewServer(logger, database).AllTasks).Methods("GET")
+
+	router.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusInternalServerError, rr.Code)
+	responseBody, err := io.ReadAll(rr.Body)
+	require.NoError(t, err)
+
+	assert.Equal(t, "task not found\n", string(responseBody))
+}
+
+// Test adding a new task
+func TestAddNewTask(t *testing.T) {
+	task := todos.NewTask("task1", "Test Task")
+	database := db.NewInMemory()
+	require.NotNil(t, database)
+
+	taskJSON, err := json.Marshal(task)
+	require.NoError(t, err)
+
+	req, err := http.NewRequest("POST", "/tasks", io.NopCloser(bytes.NewBuffer(taskJSON)))
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc("/tasks", NewServer(logger, database).AddTask).Methods("POST")
+
+	router.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusCreated, rr.Code)
+}
+
+func TestUpdateTask(t *testing.T) {
+	task := todos.NewTask("task1", "Test Task")
+	database := db.NewInMemory()
+	require.NotNil(t, database)
+
+	err := database.Add(task)
+	assert.NoError(t, err)
+
+	updatedTask := todos.NewTask("task1", "Updated Task")
+	updatedTaskJSON, err := json.Marshal(updatedTask)
+	require.NoError(t, err)
+
+	req, err := http.NewRequest("PUT", fmt.Sprintf("/tasks/%s", task.ID), bytes.NewReader(updatedTaskJSON))
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc("/tasks/{id}", NewServer(logger, database).UpdateTask).Methods("PUT")
+
+	router.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusNoContent, rr.Code)
+
+	// Verify the task was updated
+	retrievedTask, err := database.Get(task.ID)
+	require.NoError(t, err)
+	assert.Equal(t, "Updated Task", retrievedTask.Description)
+}
+
+func TestUpdateNonExistentTask(t *testing.T) {
+	updatedTask := todos.NewTask("task1", "Updated Task")
+	updatedTaskJSON, err := json.Marshal(updatedTask)
+	require.NoError(t, err)
+
+	database := db.NewInMemory()
+	require.NotNil(t, database)
+
+	req, err := http.NewRequest("PUT", "/tasks/nonexistent", io.NopCloser(bytes.NewBuffer(updatedTaskJSON)))
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc("/tasks/{id}", NewServer(logger, database).UpdateTask).Methods("PUT")
+
+	router.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusNotFound, rr.Code)
+	responseBody, err := io.ReadAll(rr.Body)
+	require.NoError(t, err)
+
+	assert.Equal(t, "task not found\n", string(responseBody))
+}
+
+func TestUpdateTaskWithInvalidData(t *testing.T) {
+	invalidTask := `{"id": "", "title": ""}` // Invalid task with empty ID and title
+	database := db.NewInMemory()
+	require.NotNil(t, database)
+
+	req, err := http.NewRequest("PUT", "/tasks/task1", io.NopCloser(bytes.NewBufferString(invalidTask)))
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc("/tasks/{id}", NewServer(logger, database).UpdateTask).Methods("PUT")
+
+	router.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusInternalServerError, rr.Code)
+	responseBody, err := io.ReadAll(rr.Body)
+	require.NoError(t, err)
+
+	assert.Equal(t, db.ErrInvalidTask.Error(), strings.Trim(string(responseBody), "\n"))
+}
+
+func TestDeleteTask(t *testing.T) {
+	task := todos.NewTask("task1", "Test Task")
+	database := db.NewInMemory()
+	require.NotNil(t, database)
+
+	err := database.Add(task)
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest("DELETE", "/tasks/task1", nil)
+	require.NoError(t, err)
+
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc("/tasks/{id}", NewServer(logger, database).DeleteTask).Methods("DELETE")
+
+	router.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusNoContent, rr.Code)
+
+	// Verify the task was deleted
+	_, err = database.Get("task1")
+	assert.Error(t, err)
+	assert.Equal(t, db.ErrNotFound, err)
 }
 
